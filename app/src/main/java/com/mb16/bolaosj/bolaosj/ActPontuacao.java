cf824b7383313b2edc5b854861b0fd2eb9be6231
Emerson <etenfen@gmail.com>
2021-07-27 14:18:53 -0300

    Atualização

--- app/src/main/java/com/mb16/bolaosj/bolaosj/ActPontuacao.java
+++ app/src/main/java/com/mb16/bolaosj/bolaosj/ActPontuacao.java
@@ -1,45 +1,37 @@
 package com.mb16.bolaosj.bolaosj;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
-import android.os.Environment;
 import android.support.v4.app.ShareCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
-import android.util.Log;
 import android.util.LruCache;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ActPontuacao extends AppCompatActivity {
     private RecyclerView mRecyclerViewPontuacao;
-    private PontuacaoAdapter adapter;
-    private List<Jogador> jogadores;
+    //private PontuacaoAdapter adapter;
+    //private List<Jogador> jogadores;
     private int porrodada;
     Spinner cbRodadaPontos;
     BancoDados db = new BancoDados(this);
-    private Jogador todosjogadores;
-    private LinearLayoutManager mLayoutManagertodosjogadores;
+    //private Jogador todosjogadores;
+    //private LinearLayoutManager mLayoutManagertodosjogadores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +41,8 @@ public class ActPontuacao extends AppCompatActivity {
         porrodada = Integer.parseInt(it.getStringExtra("rodada"));
         setTitle("Pontuação");
 
-        mRecyclerViewPontuacao = (RecyclerView) findViewById(R.id.mRecyclerViewPontuacao);
-        cbRodadaPontos = (Spinner) findViewById(R.id.cbRodadaPontos);
+        mRecyclerViewPontuacao = findViewById(R.id.mRecyclerViewPontuacao);
+        cbRodadaPontos = findViewById(R.id.cbRodadaPontos);
         cbRodadaPontos.setSelection(porrodada - 1);
 
         atualizarPontuacao(porrodada, false);
@@ -99,67 +91,33 @@ public class ActPontuacao extends AppCompatActivity {
             String criando = criaarquivo.SalvarPDF(bitmapRecycler, "pontuacao");
             if (criando.equals("sucesso")) {
                 Toast.makeText(this, "Arquivo criado com sucesso", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, criando, Toast.LENGTH_LONG).show();
+            }
 
-/*                String path = "/data/data/com.mb16.bolaosj.bolaosj/files/PDF/pontuacao.pdf";
-                Uri uri = Uri.parse(path);
-                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                //sharingIntent.setType("text/plain");
-
-                sharingIntent.setType("application/pdf");
-                //Uri uri = Uri.fromFile(pasta);
-                //sharingIntent.setDataAndType(uri, "application/pdf");
-                //sharingIntent.setData();
-                //String shareBody = "Here is the share content body";
-                //sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
-                //sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-                sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                startActivity(Intent.createChooser(sharingIntent, "Compartilhar PDF"));*/
+            return true;
+        }
+        if (id == R.id.comp) {
 
                 File documentsPath = new File(this.getFilesDir(), "mypdf");
                 File file = new File(documentsPath, "pontuacao.pdf");
                 Uri uri = FileProvider.getUriForFile(this, "com.mb16.bolaosj.bolaosj.provider", file);
 
-            /*    final Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                //shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                this.grantUriPermission("com.mb16.bolaosj.bolaosj", uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                //shareIntent.setType("application/pdf");
-                shareIntent.setDataAndType(uri, getContentResolver().getType(uri));
-                //final File pdfFile = new File(getFilesDir(), "pontuacao.pdf");
-                //shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(pdfFile));
-                startActivity(Intent.createChooser(shareIntent, "Share image using"));*/
-
-                //ERRO DE PERMISSÃO
-/*                File documentsPath = new File(this.getFilesDir(), "mypdf");
-                File file = new File(documentsPath, "pontuacao.pdf");
-                Uri uri = FileProvider.getUriForFile(this, "com.mb16.bolaosj.bolaosj.provider", file);
-
-                Intent intent = ShareCompat.IntentBuilder.from(this)
-                        //.setType("application/pdf")
-                        .setStream(uri)
-                        .setChooserTitle("Choose bar")
-                        .createChooserIntent()
-                        .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                        .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                intent.setDataAndType(uri, getContentResolver().getType(uri));
-                //this.grantUriPermission("com.mb16.bolaosj.bolaosj", uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                this.startActivity(intent);
-                */
                 try {
                     Intent intent = ShareCompat.IntentBuilder.from(this)
-                            .setStream(uri) // uri from FileProvider
-                            .setType("application/pdf")
+                            //.setStream(uri) // uri from FileProvider
+                            //.setType("application/pdf")
                             .getIntent()
                             .setAction(Intent.ACTION_SEND)
-                            .setDataAndType(uri, "pdf/*")
+                            .putExtra(Intent.EXTRA_STREAM, uri)
+                            //.setDataAndType(uri,getContentResolver().getType(uri))
+                            .setDataAndType(uri, "application/pdf")
                             .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     startActivity(intent);
 
                 } catch (Exception e){
                     e.printStackTrace();
                 }
-            } else {
-                Toast.makeText(this, criando, Toast.LENGTH_LONG).show();
-            }
 
             return true;
         }
@@ -168,6 +126,9 @@ public class ActPontuacao extends AppCompatActivity {
     }
 
     private void atualizarPontuacao(int _rodada, boolean _total) {
+        PontuacaoAdapter adapter;
+        LinearLayoutManager mLayoutManagertodosjogadores;
+
         mRecyclerViewPontuacao.setHasFixedSize(true);
         mLayoutManagertodosjogadores = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerViewPontuacao.setLayoutManager(mLayoutManagertodosjogadores);
